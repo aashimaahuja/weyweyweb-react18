4eb94507b6e9e6488c706ec961c2497a43782471
Aashima Ahuja <aashima.ahuja811@gmail.com>
2022-12-14 19:30:20 +0100

    React 18 talk added images

---
+++ SinkIn.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function SinkIn() {
+    return (
+    <div>
+        <h2>Let that Sink in...</h2>
+        <img src="http://localhost:3005/sink-in.jpeg"></img>
+    </div>)
+}
